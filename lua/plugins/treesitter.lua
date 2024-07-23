5409c84b0266a7099f8488c5660432cda7ced5cb
Duane Enriquez <duanequez@gmail.com>
2024-07-23 16:36:14 +0800

    update treesitter to use curl

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,17 +1,14 @@
 return {
-	{ -- Highlight, edit, and navigate code
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		opts = {
-			ensure_installed = { "c", "html", "lua" },
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true, disable = { "ruby" } },
-		},
-		config = function(_, opts)
-			-- prefer git instead of curl
-			require("nvim-treesitter.install").prefer_git = true
-			require("nvim-treesitter.configs").setup(opts)
-		end,
-	},
+  { -- Highlight, edit, and navigate code
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = { "c", "html", "lua" },
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true, disable = { "ruby" } },
+    },
+    config = function(_, opts)
+    end,
+  },
 }
