1db460e0839d1a8c6f7e2cff8e3678cd9f7f51b0
Duane Enriquez <duanequez@gmail.com>
2024-07-23 16:30:17 +0800

    set colorscheme based on os

--- init.lua
+++ init.lua
@@ -15,8 +15,22 @@ require("core.autocmds")
 require("lazy-bootstrap")
 require("lazy-setup")
 
--- Set colorscheme
-vim.cmd.colorscheme("catppuccin")
+-- Set colorscheme based on operating system
+local function is_macos()
+	return vim.loop.os_uname().sysname == "Darwin"
+end
+
+local function is_arch()
+	return vim.fn.filereadable("/etc/arch-release") == 1
+end
+
+if is_macos() then
+	vim.cmd.colorscheme("catppuccin")
+elseif is_arch() then
+	vim.cmd.colorscheme("everforest")
+else
+	vim.cmd.colorscheme("default")
+end
 
 -- Set neovide configuration
 if vim.g.neovide then
